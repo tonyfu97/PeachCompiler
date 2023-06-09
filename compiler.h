8889cfa76a399e9994b0ae4a93a3450e8abbb284
Tony Fu <61669380+tonyfu97@users.noreply.github.com>
2023-06-09 13:20:37 -0700

    L5. Creating our token structures

--- compiler.h
+++ compiler.h
@@ -2,6 +2,49 @@
 #define PEACHCOMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+struct pos
+{
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE,
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char *sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+    
+    // True if there is a space between this token and the next token.
+    bool whitespace;
+
+    // Points to the first character in the bracket.
+    // If not between brackets, this is NULL.
+    const char *between_brackets;
+};
 
 enum
 {
